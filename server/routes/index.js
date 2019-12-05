bea1752b8d56b94608abd699b64bbcfd3bd01c89
Aecio Cavalcanti <aecio.rcv@gmail.com>
2019-12-05 10:32:15 -0500

    clean unused files

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function(req, res, next) {
-  const price = process.env.PRICE * 10;
   res.status(200).send({ welcomeMessage: "Welcome" });
 });
 
--- server/routes/ping.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.post("/", function(req, res, next) {
-  const showStuff = process.env.stuff;
-});
-
-module.exports = router;
